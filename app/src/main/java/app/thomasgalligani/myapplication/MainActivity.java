dfcb47d7e67ea8d5dfd17c9d2e4bb821f1ea939b
swimyoda <tfgalligani@gmail.com>
2016-09-16 10:33:31 -0400

    Update MainActivity.java

--- app/src/main/java/app/thomasgalligani/myapplication/MainActivity.java
+++ app/src/main/java/app/thomasgalligani/myapplication/MainActivity.java
@@ -33,9 +33,13 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    private String output;
+    private String outputFile;
     private Resources resources;
     private EditText editText;
+    private ArrayList<String> questions = new ArrayList<String>();
+    private ArrayList<String> answers = new ArrayList<String>();
+    private ArrayList<String> words = new ArrayList<String>();
+    private ArrayList<String> questionWords = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +55,55 @@ public class MainActivity extends Activity {
     public void click(View view)
     {
         String input = getString(R.id.editText);
-
+        String[] keywords = processText(input);
         JSONObject fullFile;
         JSONArray QAs;
+        JSONArray answers;
+
+        try {
+            outputFile = LoadFile("databaseTest.json", false);
+            try {
+                fullFile = new JSONObject(outputFile);
+                QAs = fullFile.getJSONArray("question-answers");
+            } catch (JSONException e) {
+                Toast toast = Toast.makeText(this, "JSON is mesed up!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+        catch (IOException e)
+        {
+            //display an error toast message
+            Toast toast = Toast.makeText(this, "File: not found!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        try {
+            switch (keywords[0]) {
+                case "who":
+                    answers = QAs.getJSONObject(0).getJSONArray("answers");
+                    break;
+                case "what":
+                    JSONObject categories = QAs.getJSONObject(1).getJSONObject("answers");
+                    answers = categories.getJSONObject(keywords[1]).getJSONArray();
+                case "how":
+                    answers = QAs.getJSONObject(2).getJSONArray("answers");
+                    break;
+                case "why":
+                    answers = QAs.getJSONObject(3).getJSONArray("answers");
+                    break;
+                case "where":
+                    answers = QAs.getJSONObject(4).getJSONArray("answers");
+                    break;
+                case "when":
+                    answers = QAs.getJSONObject(5).getJSONArray("answers");
+                    break;
+
+            }
+        }
+        catch(JSONException e)
+        {
+            Toast toast = Toast.makeText(this, "JSON is mesed up!", Toast.LENGTH_LONG);
+            toast.show();
+        }
 
         JSONObject who;
         JSONArray answersWho;
@@ -83,6 +133,11 @@ public class MainActivity extends Activity {
                 fullFile = new JSONObject(output);
                 Log.i("try", fullFile.toString());
                 QAs = fullFile.getJSONArray("question-answers");
+
+
+
+
+
                 Log.i("try", QAs.toString());
                 who = QAs.getJSONObject(0);
                 what = QAs.getJSONObject(1);
@@ -125,9 +180,95 @@ public class MainActivity extends Activity {
 
     }
 
-    private String processText(String question)
-    {
 
+    //done--------------
+    private String[] processText(String input)
+    {
+        ArrayList<String> wordified = wordify(input);
+        String question = findKeyword(questions, wordified);
+        String[] output = new String[2];
+        switch(question)
+        {
+            case "what":
+                output[0] = question;
+                output[1] = findKeyword(wordified, whatWords);
+                break;
+            case default:
+                output[0] = question;
+                output[1] = null;
+                break;
+            return output;
+        }
+    }
+
+    private ArrayList<String> wordify(String sentence)
+    {
+        ArrayList<String> words = new ArrayList<String>();
+        int start = 0;
+        for(int i = 0; i < sentence.length(); i++)
+        {
+            if(sentence.charAt(i) == " " || i == sentence.length() - 1)
+            {
+                words.add(sentence.substring(start, i).toLowerCase());
+                i++;
+                start = i;
+            }
+        }
+        return words;
+    }
+
+    public String findKeyword(ArrayList<String> words, ArrayList<String> arr)
+    {
+        for(String word: arr)
+        {
+            for(String w: words)
+            {
+                if(word.equals(w))
+                {
+                    return word;
+                }
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<String> howQuestion()
+    {
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add("Good!");
+        answers.add("Okay");
+        answers.add("Bad!");
+        return answers;
+    }
+    public ArrayList<String> doQuestion()
+    {
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add("Yes");
+        answers.add("Maybe");
+        answers.add("No");
+        return answers;
+    }
+    public ArrayList<String> whenQuestion()
+    {
+        ArrayList<String> answers = new ArrayList<String>();
+        answers.add("Yesterday");
+        for(int i = 1; i <= 12; i++){
+            answers.add(i + " o'clock");
+        }
+        answers.add("Tomorrow");
+        return answers;
+    }
+    public ArrayList<String> whatQuestion(ArrayList<String> s)
+    {
+        String keyword = findKeyword(s, words);
+        int index;
+        for(int i = 0; i < questions.size(); i++){
+            if (questions.get(i).contains(keyword) == true){
+                index = i;
+                break;
+            }
+        }
+        return answers.get(index);
     }
 
 
